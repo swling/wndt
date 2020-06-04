242968d57357273e3afdf3a2f7716215d9fcae82
Swling <tangfou@gmail.com>
2020-06-04 18:57:36 +0800

    优化付费阅读付费下载

--- template-parts/content/content.php
+++ template-parts/content/content.php
@@ -8,21 +8,22 @@
 			 * 在内容页面判断当前用户是否已付费
 			 * 采用wp editor <!--more--> 标记区分免费部分与付费部分
 			 */
+			$with_paid_content  = false;
 			if (wnd_get_post_price($post->ID)) {
+				$content = wndt_explode_post_by_more($post->post_content);
+				$with_paid_content  = $content[1] ?? false;
 				$user_id = get_current_user_id();
 				if (wnd_user_has_paid($user_id, $post->ID) or $post->post_author == $user_id) {
 					the_content();
 				} else {
-					$content = wndt_explode_post_by_more($post->post_content);
 					echo $content[0];
 				}
 			} else {
 				the_content();
 			}
 
-			// 在内容页放置按钮
-			echo wnd_paid_reading_button($post->ID);
-			echo wnd_paid_download_button($post->ID);
+			// 在内容页放置付费按钮，将自动检测是否包含付费文件
+			echo wnd_pay_button($post->ID, $with_paid_content);
 			?>
 		</div>
 	</div>
