360182720296f992ee12eeae70ec58993e59b5cd
Swling <tangfou@gmail.com>
2020-07-06 15:51:15 +0800

    将自定义更新 API 整合为抽象类，纳入wnd-frontend

--- includes/utility/wndt-upgrader-github.php
+++ includes/utility/wndt-upgrader-github.php
@@ -8,7 +8,7 @@ use Wnd\Utility\Wnd_Upgrader_Theme;
  *@since 2020.04.13
  *主题配置
  */
-class Wndt_Upgrader extends Wnd_Upgrader_Theme {
+class Wndt_Upgrader_Github extends Wnd_Upgrader_Theme {
 
 	/**
 	 *获取更新包详细信息，需要完成如下下信息构造：
@@ -22,23 +22,23 @@ class Wndt_Upgrader extends Wnd_Upgrader_Theme {
 	 *	$this->upgrade_info['new_version'];
 	 */
 	protected function get_upgrade_info() {
-		// $url      = 'https://api.github.com/repos/swling/wndt/releases';
-		// $response = wp_remote_get($url);
-		// if (is_wp_error($response)) {
-		// 	return $response;
-		// }
+		$url      = 'https://api.github.com/repos/swling/wndt/releases';
+		$response = wp_remote_get($url);
+		if (is_wp_error($response)) {
+			return $response;
+		}
 
-		// $response = json_decode($response['body'], true);
-		// if (is_array($response)) {
-		// 	$response = current($response);
-		// }
+		$response = json_decode($response['body'], true);
+		if (is_array($response)) {
+			$response = current($response);
+		}
 
 		// 读取GitHub tag name
-		$this->remote_version = '0.01';
+		$this->remote_version = $response['tag_name'];
 
 		// 构造安装包信息
-		$this->upgrade_info['url']         = 'http://127.0.0.1/wordpress';
-		$this->upgrade_info['package']     = 'http://127.0.0.1/wordpress.zip';
+		$this->upgrade_info['url']         = $response['html_url'];
+		$this->upgrade_info['package']     = $response['zipball_url'];
 		$this->upgrade_info['theme']       = $this->directory_name;
 		$this->upgrade_info['new_version'] = $this->remote_version;
 	}
