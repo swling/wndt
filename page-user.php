5a8fee597054f1b0da03598970301e4acb581e29
Swling <tangfou@gmail.com>
2020-10-13 23:44:20 +0800

    用户中心注释

--- page-user.php
+++ page-user.php
@@ -3,8 +3,12 @@ use Wnd\Utility\Wnd_Login_Social;
 
 /**
  *Template Name: 用户中心
- *层级：一级
- *slug:ucenter
+ *
+ * 页面功能：
+ * - 根据 URL 参数 $_GET['state'] 处理社交登录（绝大部分社交登录均支持在回调 URL 中添加 $_GET['state']，如有例外后续补充处理）
+ * - 根据 URL 参数 $_GET['module'] 呈现对应 UI 模块
+ * - 根据 URL 参数 $_GET['action'] = （submit/edit） 调用对应内容发布/编辑表单模块
+ * - 默认为用户中心：注册、登录、账户管理，内容管理，财务管理等
  */
 
 $module = $_GET['module'] ?? false;
@@ -19,13 +23,16 @@ if ($state) {
 }
 
 get_header();
+
 echo '<main class="column">';
 echo '<div class="main box">';
 
+// 根据 URL 参数 $_GET['module'] 呈现对应 UI 模块
 if ($module) {
 	$class = Wnd\Controller\Wnd_API::parse_class($module, 'Module');
 	echo $class::render();
 } else {
+	// 根据 URL 参数 $_GET['action'] = （submit/edit） 调用对应内容发布/编辑表单模块
 	switch ($action) {
 	case 'submit':
 		echo Wndt\Module\Wndt_Post_Submit::render();
@@ -35,6 +42,7 @@ if ($module) {
 		echo Wndt\Module\Wndt_Post_Edit::render();
 		break;
 
+	// 默认用户中心：注册、登录、账户管理，内容管理，财务管理等
 	default:
 		echo Wndt\Module\Wndt_User_Center::render();
 		break;
